7075871fab84c672c6d9f4228bfef2938d843f7d
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-01-31 18:14:58 +0100

    added env variables

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = ''
+openai.api_key = os.environ['API_OPENAI']
 
 
 @app.route("/", methods=("GET", "POST"))
