874df60413baf952574fb59be615e3d41538a342
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-01-31 18:22:24 +0100

    corrections

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = oos.environ.get('API_URL')
+openai.api_key = oos.environ.get('API_KEY')
 
 
 @app.route("/", methods=("GET", "POST"))
