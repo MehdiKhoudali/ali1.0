7771191205e1afe435e564723cc6f62929692cc0
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-01-31 18:39:13 +0100

    hello

--- api/index.py
+++ api/index.py
@@ -14,7 +14,7 @@ def index():
             model="text-davinci-003",
             prompt= animal,
             temperature=0.7,
-            max_tokens=4000,
+            max_tokens=2000,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
