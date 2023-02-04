1f4eca3defb2fbf8b5c63b695b5a3ccc883a1012
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-02-04 17:02:23 +0100

    api key correction

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = os.getenv("API_KEY")
 
 @app.route("/", methods=("GET", "POST"))
 def index():
