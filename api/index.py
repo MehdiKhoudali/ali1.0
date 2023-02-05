96e619ea93e7770cc6d27be944b9bf435ca2a55b
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-02-05 11:31:41 +0100

    added the image generationé

--- api/index.py
+++ api/index.py
@@ -34,5 +34,20 @@ def queston_to_code():
     result = request.args.get("result")
     return render_template('code.html', result=result)
 
+@app.route('/image_generation', methods=('GET', 'POST'))
+def image_generation():
+    if request.method == "POST":
+        animal = request.form['user_input']
+        response = openai.Image.create(
+          prompt=animal,
+          n=3,
+          size="256x256",
+        )
+        return redirect(url_for('image_generation', result=response['data'][0]['url']))
+    result = request.args.get("result")
+    return render_template('image.html', result=result)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
